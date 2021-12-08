1decfbd3d6a3f7fbf8d3c8897eff8464c07b1d45
Julgargal <juliagarciagallego@gmail.com>
2021-12-08 17:38:37 +0000

    Added support to nedb for POST method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var DataStore = require('nedb');
 
-var port = 3000;
+var port = 3001;
 var BASE_APPI_PATH = "/api/v1";
 var DB_FILE_NAME= __dirname + "/usuarios.json"
 
@@ -11,20 +11,31 @@ console.log("Starting API server...");
 var app = express();
 app.use(bodyParser.json()); 
 
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+})
+
 app.get("/", (req, res) => {
     res.send("<html><body><h1>My server</html></body></h1>");
 });
 
 app.get(BASE_APPI_PATH + "/usuarios", (req, res) => {
-    console.log(Date() + " - GET /contacts");
-    res.send(usuarios);
+    console.log(Date() + " - GET /usuarios");
+    res.send([]);
 });
 
 app.post(BASE_APPI_PATH + "/usuarios", (req, res) => {
-    console.log(Date() + " - POST /contacts");
+    console.log(Date() + " - POST /usuarios");
     var usuario = req.body;
-    usuarios.push(usuario);
-    res.sendStatus(201);
+    db.insert(usuario, (err) => {
+        if(err){
+            console.log(Date() + " - " + err);
+            res.sendStatus(500);
+        }else{
+            res.sendStatus(201);
+        }
+    });
 });
 
 app.listen(port);
