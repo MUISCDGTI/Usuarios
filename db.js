eda924ae4c10a467416c27057da76ec79ca029a3
gfPedro <36966522+gfPedro@users.noreply.github.com>
2021-12-27 09:38:26 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = (process.env.MONGO_URL || 'mongodb+srv://User0:QCm9ztEoSFF1rUXX@cluster0.qsctg.mongodb.net/test');
+const DB_URL = (process.env.MONGO_URL || 'mongodb+srv://User0:CGovoJ24U1tl6MNO@cluster0.qsctg.mongodb.net/test');
 
 const dbConnect = function () {
 
