f35a84317b4dcf04a8ba4bfefb33a45bd42b9431
gfPedro <gfpedro97@gmail.com>
2022-01-18 07:49:18 +0100

    Fix

--- authenticationMiddleware.js
+++ authenticationMiddleware.js
@@ -1,6 +1,8 @@
 
 const jwt = require('jsonwebtoken');
 
+const ACCESS_TOKEN_SECRET = (process.env.ACCESS_TOKEN_SECRET || "CHANGE_THIS_SUPER_SECRET_ACCESS_TOKEN_SECRET");
+
 function authenticateTokenMiddleware(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
